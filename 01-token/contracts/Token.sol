7c7a8cd64f9c635917a06a3851b94db9f9156c1c
Denys Proskura <57964380+MrJeleika@users.noreply.github.com>
2026-05-21 01:14:27 +0200

    Update Token.sol

--- 01-token/contracts/Token.sol
+++ 01-token/contracts/Token.sol
@@ -8,36 +8,65 @@ contract Token is IERC20 {
     string public symbol;
     uint8 public constant decimals = 18;
 
-    // TODO: declare storage for totalSupply, balances, and allowances.
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    error InsufficientBalance();
+    error InsufficientAllowance();
+    error TransferToZeroAddress();
+    error ApproveToZeroAddress();
 
     constructor(string memory name_, string memory symbol_, uint256 initialSupply) {
         name = name_;
         symbol = symbol_;
-        // TODO: credit `initialSupply` to msg.sender and emit a Transfer event
-        //       from the zero address to msg.sender for the same amount.
+        _totalSupply = initialSupply;
+        _balances[msg.sender] = initialSupply;
+        emit Transfer(address(0), msg.sender, initialSupply);
     }
 
     function totalSupply() external view returns (uint256) {
-        // TODO
+        return _totalSupply;
     }
 
     function balanceOf(address account) external view returns (uint256) {
-        // TODO
+        return _balances[account];
     }
 
     function allowance(address owner, address spender) external view returns (uint256) {
-        // TODO
+        return _allowances[owner][spender];
     }
 
     function transfer(address to, uint256 value) external returns (bool) {
-        // TODO
+        _transfer(msg.sender, to, value);
+        return true;
     }
 
     function approve(address spender, uint256 value) external returns (bool) {
-        // TODO
+        if (spender == address(0)) revert ApproveToZeroAddress();
+        _allowances[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
     }
 
     function transferFrom(address from, address to, uint256 value) external returns (bool) {
-        // TODO
+        uint256 current = _allowances[from][msg.sender];
+        if (current < value) revert InsufficientAllowance();
+        unchecked {
+            _allowances[from][msg.sender] = current - value;
+        }
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 value) private {
+        if (to == address(0)) revert TransferToZeroAddress();
+        uint256 fromBalance = _balances[from];
+        if (fromBalance < value) revert InsufficientBalance();
+        unchecked {
+            _balances[from] = fromBalance - value;
+        }
+        _balances[to] += value;
+        emit Transfer(from, to, value);
     }
 }
